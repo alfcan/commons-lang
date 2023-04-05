99c7684922006cc8ed80469537db7505f08e27f6
Kevin Pacifico <k.pacifico@studenti.unisa.it>
2023-04-05 14:19:29 +0200

    Comment unused import
    Comment unude import at line 23 in class ArchUtilsTest.java

--- src/test/java/org/apache/commons/lang3/ArchUtilsTest.java
+++ src/test/java/org/apache/commons/lang3/ArchUtilsTest.java
@@ -20,7 +20,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertNull;
+//import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.apache.commons.lang3.arch.Processor;
